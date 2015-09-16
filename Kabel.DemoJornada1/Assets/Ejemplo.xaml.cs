94dc0f1243b19edcc90e1b07d407e152410ae09e
rfcm83 <rfcm83@gmail.com>
2015-09-16 10:01:24 +0200

    Demo J1

---
+++ Kabel.DemoJornada1/Assets/Ejemplo.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kabel.DemoJornada1.Assets
+{
+    /// <summary>
+    /// Interaction logic for Ejemplo.xaml
+    /// </summary>
+    public partial class Ejemplo : UserControl
+    {
+        public Ejemplo()
+        {
+            InitializeComponent();
+        }
+    }
+}
