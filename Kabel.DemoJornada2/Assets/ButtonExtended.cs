365cbfb13463be41c6f964ddd2e8f739818d301b
rfcm83 <rfcm83@gmail.com>
2015-09-18 12:04:25 +0200

    Demo J2

---
+++ Kabel.DemoJornada2/Assets/ButtonExtended.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Kabel.DemoJornada2.Assets
+{
+    public class ButtonExtended : Button
+    {
+        private int _clicks;
+
+        public ButtonExtended()
+        {
+            this.Click += Clicked;
+        }
+
+        public static readonly DependencyProperty IsBusyProperty = DependencyProperty.Register(
+            "IsBusy", typeof(bool), typeof(ButtonExtended), new PropertyMetadata(default(bool)));
+
+        public bool IsBusy
+        {
+            get { return (bool)GetValue(IsBusyProperty); }
+            set { SetValue(IsBusyProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
+            "Icon", typeof(ImageSource), typeof(ButtonExtended), new PropertyMetadata(default(ImageSource)));
+
+        public ImageSource Icon
+        {
+            get { return (ImageSource)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static readonly DependencyProperty ClicksProperty = DependencyProperty.Register(
+            "Clicks", typeof(string), typeof(ButtonExtended), new PropertyMetadata(default(string)));
+
+        public string Clicks
+        {
+            get { return (string)GetValue(ClicksProperty); }
+            set { SetValue(ClicksProperty, value); }
+        }
+
+        private void Clicked(object sender, RoutedEventArgs e)
+        {
+            if (sender != null)
+            {
+                _clicks++;
+                Clicks = _clicks.ToString();
+            }
+        }
+    }
+}
---
+++ Kabel.DemoJornada2/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Kabel.DemoJornada2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string _texto;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.MouseEnter += OnMouseEnter;
+            this.DataContext = this;
+        }
+
+        public string Texto
+        {
+            get { return _texto; }
+            set
+            {
+                _texto = value;
+                OnPropertyChanged("Texto");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            Texto += "hola";
+        }
+    }
+}
